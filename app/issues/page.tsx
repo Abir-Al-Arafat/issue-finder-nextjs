05ddd4c1ec3a1d14a7bb6fc2eeb9f5d0869ae470
Abir-Al-Arafat <antorabir@gmail.com>
2024-05-08 16:19:34 +0600

    Issue Page: Input fields and buttons added

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPage = () => {
   return (
     <div>
-      <Button>NEW</Button>
+      <Button>
+        <Link href={`/issues/new`}>New</Link>
+      </Button>
     </div>
   );
 };
