e8c322ab5382d9900653aa9d791ebfd5f397b2db
Abir-Al-Arafat <antorabir@gmail.com>
2024-05-21 13:08:26 +0600

    Delete Button added

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,14 +1,2 @@
-import { Skeleton } from "@/app/components";
-import { Box } from "@radix-ui/themes";
-import "easymde/dist/easymde.min.css";
-
-const NewIssuePage = () => {
-  return (
-    <Box className="max-w-xl ">
-      <Skeleton height="30px" />
-      <Skeleton height="371px" />
-    </Box>
-  );
-};
-
-export default NewIssuePage;
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
+export default IssueFormSkeleton;
