374243909ce47f714c517303d7870edcff6203c1
Abir-Al-Arafat <antorabir@gmail.com>
2024-08-14 00:43:28 +0600

    component added for showing issue summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return <LatestIssues />;
+  const openCount = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgressCount = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closedCount = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return (
+    <>
+      {" "}
+      <LatestIssues />
+      <IssueSummary
+        open={openCount}
+        inProgress={inProgressCount}
+        closed={closedCount}
+      />
+    </>
+  );
 }
