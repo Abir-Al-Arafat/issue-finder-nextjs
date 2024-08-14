bf162be67522ee64a07f14121927ac6e5629bfa9
Abir-Al-Arafat <antorabir@gmail.com>
2024-08-14 23:00:43 +0600

    optimization: performance enhanced using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -10,17 +10,21 @@ import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 import AssigneeSelect from "./AssigneeSelect";
 import { Metadata } from "next/types";
+import { cache } from "react";
 
 interface Props {
   params: { id: string };
 }
-
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
 const IssueDetailPage = async ({ params }: Props) => {
   if (isNaN(parseInt(params.id))) notFound();
   const session = await getServerSession(authOptions);
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  // const issue = await prisma.issue.findUnique({
+  //   where: { id: parseInt(params.id) },
+  // });
+  const issue = await fetchIssue(parseInt(params.id));
 
   if (!issue) notFound();
   // await delay(2000);
@@ -43,9 +47,10 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  // const issue = await prisma.issue.findUnique({
+  //   where: { id: parseInt(params.id) },
+  // });
+  const issue = await fetchIssue(parseInt(params.id));
   return {
     title: `Issue ${issue?.title} | Issue Tracker`,
     description: `Details of issue ${issue?.id}`,
